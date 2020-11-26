00f958d698309d33a46d5cd912c4e6fce29d735b
Wendel Bandeira <besugos@gmail.com>
2020-11-25 22:32:50 -0300

    Splash screen

--- app/src/main/java/com/besugos/desafio3dha/SplashActivity.kt
+++ app/src/main/java/com/besugos/desafio3dha/SplashActivity.kt
@@ -1,11 +1,25 @@
 package com.besugos.desafio3dha
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
