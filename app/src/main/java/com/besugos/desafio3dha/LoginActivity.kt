726891bc99bebb168aaf9ff49101d8581b2f0fcf
Wendel Bandeira <besugos@gmail.com>
2020-11-26 15:02:09 -0300

    Register layout

--- app/src/main/java/com/besugos/desafio3dha/LoginActivity.kt
+++ app/src/main/java/com/besugos/desafio3dha/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.besugos.desafio3dha
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin = this.findViewById<Button>(R.id.btnLoginLogin)
+        val btnRegister = this.findViewById<TextView>(R.id.txtCreateAccount)
+
+        btnLogin.setOnClickListener() {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegister.setOnClickListener() {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
